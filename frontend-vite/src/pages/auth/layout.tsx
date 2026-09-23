2deecd994b890c720bca698f65042bf88df5d092
Bau <bautistadelprato16@gmail.com>
2026-09-23 14:08:15 -0300

    fix: remove duplicated project directory

--- frontend-vite/src/pages/auth/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export default function AuthLayout() {
-  return (
-    <div className="flex min-h-dvh flex-col items-center justify-center bg-[#150224] px-4">
-      <Outlet />
-    </div>
-  )
-}
